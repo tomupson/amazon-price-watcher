47e4c808c983f01881c0e3472513ab3c7cf2ba8d
Tom Upson <30987275+UppyMeister@users.noreply.github.com>
2019-02-12 21:41:33 +0000

    Change project type

--- apw/Program.cs
+++ apw/Program.cs
@@ -2,11 +2,14 @@
 
 namespace apw
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
         }
     }
 }
